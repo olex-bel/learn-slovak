62d433ae3578c05dc9bef08b741aa3c6c7320fa4
olex-bel <ban_konung@ukr.net>
2024-04-19 22:02:04 +0200

    Fixed build issue

--- src/pages/Words.tsx
+++ src/pages/Words.tsx
@@ -1,43 +1,13 @@
-import { useState } from "react";
 import Typography from "@mui/material/Typography";
-import Fab from "@mui/material/Fab";
-import AddIcon from "@mui/icons-material/Add";
 import LearnWordsMenu from "../features/words/LearnWordsMenu";
-import AddWordDialog from "../features/words/dialogs/AddWordDialog";
 
-export function Component() {
-    const [modalOpen, setModalOpen] = useState<boolean>(false);
-
-    const handleClose = () => {
-        setModalOpen(false);
-    };
-
-    const handleOpen = () => {
-        setModalOpen(true);
-    };
-    
+export function Component() {    
     return (
         <>
             <Typography variant="h3" sx={{ textAlign: "center", mt: 2, mb: 4 }}>
                 Словацький словник.
             </Typography>
             <LearnWordsMenu />
-
-            <Fab 
-                size="medium" 
-                color="primary" 
-                aria-label="add" 
-                sx={{
-                    position: "absolute",
-                    right: 10,
-                    bottom: 10,
-                }}
-                onClick={handleOpen}
-            >
-                <AddIcon />
-            </Fab>
-
-            <AddWordDialog open={modalOpen} onClose={handleClose} />
         </>
     );
 }
